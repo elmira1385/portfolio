c73f02aa0f8b3a3a47c2836a107417e2ace0a8ee
elmira1385 <elmirashokriyan64@gmail.com>
2026-02-07 00:37:04 +0330

    baseurl

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance=axios.create({
-    baseURL:"http://localhost:3000/"
+    baseURL:"https://elmirashokriyan.ir/"
 })
 export default instance
 
-// https://elmirashokriyan.ir/
